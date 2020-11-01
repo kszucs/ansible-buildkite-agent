5b87af18521c72700cc5a16492f81cd1c5e202c6
Peter Mounce <petermounce@improbable.io>
2020-11-01 12:19:54 +0000

    recon

--- workflow/incremental-pre-commit.sh
+++ workflow/incremental-pre-commit.sh
@@ -3,6 +3,11 @@
 source "$(dirname "${BASH_SOURCE[0]}")/lib/stdlib.bash" || exit 67
 
 log_action "Running pre-commit, incrementally. (You can supply pre-commit flags to this script)..."
+
+git branch -a
+git fetch --all
+git branch -a
+
 pre-commit run \
   --from-ref "origin/develop" \
   --to-ref "HEAD" \
