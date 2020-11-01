c8bb24d6e52d72e3c2b235022fae76b34038fbff
Peter Mounce <petermounce@improbable.io>
2020-11-01 13:15:47 +0000

    unnecessary

--- workflow/incremental-pre-commit.sh
+++ workflow/incremental-pre-commit.sh
@@ -4,10 +4,6 @@ source "$(dirname "${BASH_SOURCE[0]}")/lib/stdlib.bash" || exit 67
 
 log_action "Running pre-commit, incrementally. (You can supply pre-commit flags to this script)..."
 
-# We need inside CI to have more of the repository to be able to incrementally lint.
-# https://docs.travis-ci.com/user/customizing-the-build/#git-clone-depth
-[[ "${CI:-}" == "true" ]] && git fetch --unshallow
-
 pre-commit run \
   --from-ref "origin/develop" \
   --to-ref "HEAD" \
